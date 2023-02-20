d63baa8be691fec9ff664958e3f08c8b07fad612
David <dmdcondon@gmail.com>
2023-02-20 18:22:27 +0000

    Adding files for testing + some commenting

--- CustomerDriver.java
+++ CustomerDriver.java
@@ -1,19 +1,24 @@
 public class CustomerDriver{
     public static void main(String[] args) {
-        Movie Morbius = new Movie("Morbius", Movie.CHILDREN);
-        Movie Encanto = new Movie("Encanto", Movie.REGULAR);
+        // creating 3 movies that will be rented by a customer 
+        Movie Encanto = new Movie("Encanto", Movie.CHILDREN);
+        Movie Morbius = new Movie("Morbius", Movie.REGULAR);
         Movie Aftersun = new Movie("Aftersun", Movie.NEW_RELEASE); 
 
+        // creating a customer that will rent out the movies above
         Customer customer = new Customer("John Doe");
 
-        Rental rental1 = new Rental(Morbius, 4);
-        Rental rental2 = new Rental(Encanto, 2);
-        Rental rental3 = new Rental(Aftersun, 3);
+        // creating rentals for the above movies with how long they were rented for 
+        Rental rental1 = new Rental(Encanto, 4);
+        Rental rental2 = new Rental(Morbius, 3);
+        Rental rental3 = new Rental(Aftersun, 2);
 
+        // adding the rentals to the customer showing that they are renting the movies
         customer.addRental(rental1);
         customer.addRental(rental2);
         customer.addRental(rental3);
 
+        // prints out the receipt for the rentals 
         System.out.print(customer.statement());
 
     }
